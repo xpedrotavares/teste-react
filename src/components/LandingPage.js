51c2dcd83b45e4b3472d291056f4610a1a31a032
Pedro Tavares <xpedrotavaresdev@gmail.com>
2021-01-08 20:13:01 -0300

    components/ created. LandingPage component created

---
+++ src/components/LandingPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import bgLanding from '../assets/images/bg.jpg';
+import '../assets/styles/body.css'
+import '../assets/styles/LandingPage.css';
+
+const LandingPage = () => {
+    return(
+        <div className='landingpage-container'>
+            {/* <img src={bgLanding} /> */}
+        </div>
+    )
+}
+
+export default LandingPage;
