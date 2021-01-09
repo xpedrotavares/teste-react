c5610cf3a6483a1e0c02aee5ebbf6dd458f9bae9
Pedro Tavares <xpedrotavaresdev@gmail.com>
2021-01-09 00:17:13 -0300

    landingpage for mobile screen done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
-import LandingPage from './components/LandingPage'
+import React from 'react';
+
 import './assets/styles/body.css'
+import "bootstrap/dist/css/bootstrap.css";
+
+import LandingPage from './components/LandingPage'
+
 function App() {
   return (
     <div className="App">
--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,14 +1,55 @@
-import React from 'react';
-import bgLanding from '../assets/images/bg.jpg';
-import '../assets/styles/body.css'
-import '../assets/styles/LandingPage.css';
+import React from "react";
+
+import "../assets/styles/body.css";
+import "../assets/styles/LandingPage.css";
+
+import InputGroup from "react-bootstrap/InputGroup";
+import Button from "react-bootstrap/Button";
+import FormControl from "react-bootstrap/FormControl";
+
+import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+import logoNegative from "../assets/images/logo-white.jpg";
 
 const LandingPage = () => {
-    return(
-        <div className='landingpage-container'>
-            {/* <img src={bgLanding} /> */}
-        </div>
-    )
-}
+  return (
+    <div className="landingpage-container">
+      <div className="negative-logo-div d-flex justify-content-center align-items-center">
+        <img className="negative-logo" src={logoNegative} alt="background" />
+      </div>
+
+      <div className="form-div d-flex flex-column justify-content-center align-items-center">
+            <div className="div-h1 mb-2">
+
+          <h1 > Descubra os melhores <br/> restaurantes da sua cidade</h1>
+          
+            </div>
+            
+        <InputGroup className="mb-3">
+        <InputGroup.Prepend>
+
+      <InputGroup.Text>
+      <FontAwesomeIcon icon={faMapMarkerAlt} />
+      </InputGroup.Text>
+    </InputGroup.Prepend>
+          <FormControl
+        //   {}
+           
+            placeholder='Digite sua cidade'
+            aria-label="Digite a sua cidade"
+            aria-describedby="basic-addon2"
+          />
+  
+              
+          
+          <InputGroup.Append>
+            <Button className="ml-1" variant="success"> Buscar</Button>
+          </InputGroup.Append>
+        </InputGroup>
+      </div>
+    </div>
+  );
+};
 
 export default LandingPage;
